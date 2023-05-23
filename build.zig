c9796aa9f0311e367c16b8dcc0dbcae1f9e48f9a
Morten Linderud <morten@linderud.pw>
2023-05-23 17:14:09 +0200

    Fixed build.zig
    Signed-off-by: Morten Linderud <morten@linderud.pw>

--- build.zig
+++ build.zig
@@ -7,24 +7,14 @@ pub fn build(b: *Builder) void {
         .os_tag = .freestanding,
         .abi = .none,
         .cpu_arch = .riscv32,
-        .cpu_model = .{
-            .explicit = &std.Target.riscv.cpu.generic_rv32,
-        },
     });
     elf.setBuildMode(.ReleaseSmall);
-    elf.setLinkerScriptPath(.{ .path = "../../tkey-libs/app.lds" });
-    elf.addAssemblyFileSource(.{ .path = "../../tkey-libs/libcrt0/crt0.S" });
-
-    if (b.option(bool, "emit-elf", "Should an ELF file be emitted in the current directory?") orelse false) {
-        elf.setOutputDir(".");
-    }
+    elf.setLinkerScriptPath(.{ .path = "src/app.lds" });
+    elf.addAssemblyFileSource(.{ .path = "src/crt0.S" });
 
     const binary = b.addSystemCommand(&[_][]const u8{
         b.option([]const u8, "objcopy", "objcopy executable to use (defaults to riscv64-unknown-elf-objcopy)") orelse "llvm-objcopy",
-        "-I",
-        "elf32-littleriscv",
-        "-O",
-        "binary",
+        "-I", "elf32-littleriscv", "-O", "binary",
     });
     binary.addArtifactArg(elf);
     binary.addArg("riscv-zig-blink.bin");
